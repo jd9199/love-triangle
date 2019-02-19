3564284b67ccb5017fe249b4d93709cf98b49d99
jd9199 <grafclown999@gmail.com>
2019-02-20 00:09:02 +0300

    Complete love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;  
+  for (let i = 0, length = preferences.length; i<length; i++) { 
+      const second = preferences[i] - 1; 
+      if(second === i){
+       continue; 
+      }       
+      const third = preferences[second] - 1;   
+      if(third === second){
+       continue;
+      }
+      if(i === preferences[third] - 1){
+      count++;
+      }
+    } 
+   return count/3;
 };
